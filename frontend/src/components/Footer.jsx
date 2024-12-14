3395868d85ec4ece451fea780a3020a17aa8df55
Muhammad Sheraz <asifmehmood6090@gmail.com>
2024-12-14 15:13:14 +0500

    add icon

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             
             <div>
-                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <img src={assets.seraphic} className=' w-16' alt="" />
                 <p className='w-full md:w-2/3 text-gray-600'>
                 Seraphic Scent is a fragrance brand that blends rare, luxurious ingredients to create divine-inspired perfumes, offering an elegant, serene experience that transports the wearer to a world of purity, grace, and tranquility.
                 </p>
